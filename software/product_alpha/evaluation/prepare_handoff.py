c7812bb71ae480226a5e6c05a8b1b1e24ae6ce98
Rhodan-lab <epicvv1@gmail.com>
2026-08-02 17:35:38 +0700

    Roll back partial handoff publication

--- software/product_alpha/evaluation/prepare_handoff.py
+++ software/product_alpha/evaluation/prepare_handoff.py
@@ -334,6 +334,10 @@ def write_handoff(
         temporary_paths[1].write_bytes(markdown_bytes)
         os.replace(temporary_paths[0], json_path)
         os.replace(temporary_paths[1], markdown_path)
+    except Exception:
+        json_path.unlink(missing_ok=True)
+        markdown_path.unlink(missing_ok=True)
+        raise
     finally:
         for path in temporary_paths:
             path.unlink(missing_ok=True)
